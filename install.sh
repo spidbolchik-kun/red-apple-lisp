6fe9b519b7f2355a3f52e59d80cbed54bc977a58
scordatura <artemmyakota1@gmail.com>
2025-07-15 18:03:58 +0500

    installer fix and chmod

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/bash
+
 set -e
 eval RA_PATH=$(cat library-path)
-mkdir -p "$RA_PATH"
+[ -d "$RA_PATH" ] || mkdir -p "$RA_PATH"
 gsc -prelude "(define library-path \"$RA_PATH\")" mspm.scm
 mv mspm.o1 "$RA_PATH"
 gsc -exe -prelude "(define library-path \"$RA_PATH\")" -o ra ra.scm
-mkdir ~/.local/bin
+[ -d ~/.local/bin ] || mkdir ~/.local/bin
 mv ra ~/.local/bin/
 cp runtime.scm "$RA_PATH"
 cp stdlib/builtins.ra "$RA_PATH"
